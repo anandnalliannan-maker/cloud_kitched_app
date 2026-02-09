93c5c0a2cfec9bcfa3a6895a98e09ec9b2b7191a
Anand Nalliannan <anandnalliannan@gmail.com>
2026-02-10 04:40:50 +0530

    fix: import customer menu

--- lib/screens/home/customer_home.dart
+++ lib/screens/home/customer_home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../customer/customer_profile.dart';
+import '../customer/customer_menu.dart';
 import '../role_select_screen.dart';
 
 class CustomerHomeScreen extends StatelessWidget {
